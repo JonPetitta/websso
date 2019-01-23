c35ac6edd9cee103181150c11af9d410da8a71ce
Jon Petitta <jonpetitta@gmail.com>
2019-01-23 00:31:16 -0500

    angular auth nav init

---
+++ frontend/src/app/auth-nav/auth-nav.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-nav',
+  templateUrl: './auth-nav.component.html',
+  styleUrls: ['./auth-nav.component.styl']
+})
+export class AuthNavComponent implements OnInit {
+
+  @Input() title: string;
+
+  home = {
+    link:'/',
+    name:'Home',
+    role: 'user'
+  }
+
+  user = {
+    link:'/user',
+    name:'User',
+    role: 'user'
+  }
+
+  sup = {
+    link:'/sup',
+    name:'Sup',
+    role: 'supervisor'
+  }
+
+  admin = {
+    link:'/admin',
+    name:'Admin',
+    role: 'administrator'
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
